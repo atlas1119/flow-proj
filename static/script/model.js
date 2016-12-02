3bf8ef1bf7252585b99bf364638971578ec373cd
wangshaojun <wangshaojun@zrcaifu.com>
2016-12-02 18:35:27 +0800

    增加删除功能

--- static/script/model.js
+++ static/script/model.js
@@ -4,4 +4,33 @@ $(function(){
         var id = $("#workNodeSelect option:selected").val();
         window.location.href = "/model/createtemplate?id="+id;
     });
+
+    $(".default-btn").click(function(){
+        var id = $(this).attr('data-id');
+        $.ext_dialog.open({
+            width: 300,
+            haml: '<div style="text-align:center;margin-bottom:30px;">您确定要删除吗？</div>',
+            buttons:{
+                "确定":function(){
+                    $.post('deletemodel',{id:id},function(data){
+                        if(data.success){
+                            $.ext_dialog.open({
+                                width: 300,
+                                haml: '<div style="text-align:center;margin-bottom:30px;">删除成功！</div>',
+                                buttons:{
+                                    "确定":function(){
+                                        window.location.reload();
+                                    }
+                                }
+                            });
+                        }
+                    })
+                },
+                "取消": function(){
+                    $.ext_dialog.close();
+                }
+            }
+        });
+        return false;
+    });
 });
